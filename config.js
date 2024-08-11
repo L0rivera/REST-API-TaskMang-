63c35692ec6659234529fe08c07d078dc595ed29
Luis098rivera <luis.rivera040606@gmail.com>
2024-08-10 22:56:56 -0500

    Edit config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ import neo4j from "neo4j-driver";
 
 dotenv.config();
 
-const neo4jUri = process.env.NEO4J_URI;
-const neo4jUser = process.env.NEO4J_USER;
-const neo4jPass = process.env.NEO4J_PASS; 
+const neo4jUri = process.env.NEO4J_URI || 'neo4j://localhost';
+const neo4jUser = process.env.NEO4J_USER || 'neo4j';
+const neo4jPass = process.env.NEO4J_PASS || 'neo_23cont0d'; 
 
 const driver = neo4j.driver(neo4jUri, neo4j.auth.basic(neo4jUser, neo4jPass));
 
